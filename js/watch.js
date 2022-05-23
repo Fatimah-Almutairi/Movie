aed23e0200014f18bb6d8136dd75e0c90aa34d67
fatimah06 <fatima.m.mutairi@gmail.com>
2022-05-23 23:59:20 +0300

    Movie

---
+++ js/watch.js
@@ -0,0 +1,36 @@
+const Api_Key = '2d6b3291586411f85a61201ca446cbb8';
+const Api_Img= 'https://image.tmdb.org/t/p/w500';
+let watchList= [];
+
+
+JSON.parse(localStorage.getItem("watchList")).forEach(element => {
+    axios.get(`https://api.themoviedb.org/3/movie/${element}?api_key=${Api_Key}&append_to_response=videos,similar,credits`)
+    .then((res) => {
+        watchList.push(res.data)
+        document.getElementById("w").innerHTML = watchList.map(item => 
+            `
+            <div class ="col-4 ms-3">
+            <div class= "card">
+            <img src="${Api_Img+ item.poster_path}" alt="${item.title}">
+            <a class= "btn-remove" onclick="remove(${item.id})"><i class="fa fa-xmark pe-2 pt-2 fa-xl"></i></a>
+            </div>
+            </div>    
+          `
+        ).join('');
+    })
+});
+
+function remove(movie_id){
+    watchList = JSON.parse(localStorage.getItem("watchList"))
+    watchList.splice(watchList.indexOf(movie_id),1);
+    localStorage.setItem('watchList',JSON.stringify(watchList))
+    location.reload();
+}
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+
+})
+
